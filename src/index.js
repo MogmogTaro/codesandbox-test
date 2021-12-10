8f45bb84b7b780813ce40b6d23b0d389483ae269
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-10 12:07:02 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -108,3 +108,9 @@
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}です`;
 // console.log(message4)
+
+/**
+ * デフォルト値,引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayHello();
