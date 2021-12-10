14b67add5ad29072121f0730115f8c7136b2ce74
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-10 11:44:51 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -42,7 +42,27 @@
 // console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "うちだま";
+// const age = "28";
+
+// // 「私の名前はうちだまです。年齢は28歳です。」
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
